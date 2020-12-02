e1812a13af6e3a3c64b045f80193156663fd887f
Otto Harju <otto.harju@gmail.com>
2020-12-02 10:59:37 +0200

    ephemeral get and insert

--- ephemeral/src/lib.rs
+++ ephemeral/src/lib.rs
@@ -1,41 +1,58 @@
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::hash::Hash;
 
-enum Value {
-    Integer(u64),
-    SignedInteger(i64),
-    Phrase(String),
+struct Node<K,V> {
+    leaf: Option<V>,
+    branches: HashMap<K, Box<Node<K,V>>>,
 }
 
-struct Node {
-    leaf: Option<Arc<Value>>,
-    branches: HashMap<String, Box<Node>>,
-}
-
-impl Node {
-    fn new() -> Node {
-        Node{leaf: None, branches: HashMap::new()}
+impl<K,V> Node<K,V> 
+    where V: Copy, K: Eq+Hash {
+    fn new() -> Node<K,V> {
+        Node{
+            leaf: None, 
+            branches: HashMap::new()
+        }
     }
 
-    fn get(&self, path: Vec<&str>, root: &Node) -> Option<Arc<Value>> {
-        let mut iteratee = root;
+    fn get(&self, path: Vec<K>) -> &Option<V> {
+        let mut iteratee = self;
         for fragment in path {
-            match iteratee.branches.get(fragment) {
-                None => return None,
-                Some(node) => iteratee = &node,
+            match iteratee.branches.get(&fragment) {
+                None => return &None,
+                Some(node) => {
+                    iteratee = &node;
+                },
             }
         }
-        iteratee.leaf.clone()
+        &iteratee.leaf
     }
 
-    // TODO put & delete
-
+    fn insert(&mut self, path: Vec<K>, value: V) -> Option<V> {
+        let mut iteratee = self;
+        for fragment in path {
+            iteratee = iteratee.branches.entry(fragment).or_insert(Box::new(Node::new()));
+        }
+        let old_value = iteratee.leaf;
+        iteratee.leaf = Some(value);
+        old_value
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::Node;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn insert_get() {
+        let mut puu = Node::new();
+        let result = puu.insert(vec!["eka", "toka", "vika"], 42);
+        assert_eq!(result, None);
+        let result = puu.get(vec!("eka", "toka", "vika"));
+        assert_eq!(result.unwrap(), 42);
+        let result = puu.insert(vec!["eka", "toka", "vika"], 13);
+        assert_eq!(result.unwrap(), 42);
+        let result = puu.get(vec!("eka", "toka", "vika"));
+        assert_eq!(result.unwrap(), 13);
     }
 }
