cc96d5cec57f14961dc44a001cd2471961b56ab0
Otto Harju <otto.harju@gmail.com>
2020-12-03 20:04:35 +0200

    more implementations

--- ephemeral/src/lib.rs
+++ ephemeral/src/lib.rs
@@ -1,37 +1,39 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-struct Node<K,V> {
+/// PathTree represents a tree structure where every value is identified
+/// by a list of keys called a path.
+pub struct PathTree<K,V> {
     leaf: Option<V>,
-    branches: HashMap<K, Box<Node<K,V>>>,
+    branches: HashMap<K,Box<PathTree<K,V>>>,
 }
 
-impl<K,V> Node<K,V> 
-    where V: Copy, K: Eq+Hash {
-    fn new() -> Node<K,V> {
-        Node{
+impl<K,V> PathTree<K,V> 
+    where V: Copy, K: Copy+Eq+Hash {
+    pub fn new() -> PathTree<K,V> {
+        PathTree{
             leaf: None, 
             branches: HashMap::new()
         }
     }
 
-    fn get(&self, path: Vec<K>) -> &Option<V> {
+    pub fn get(&self, path: &Vec<K>) -> &Option<V> {
         let mut iteratee = self;
         for fragment in path {
             match iteratee.branches.get(&fragment) {
                 None => return &None,
-                Some(node) => {
-                    iteratee = &node;
+                Some(path_tree) => {
+                    iteratee = &path_tree;
                 },
             }
         }
         &iteratee.leaf
     }
 
-    fn insert(&mut self, path: Vec<K>, value: V) -> Option<V> {
+    pub fn insert(&mut self, path: &Vec<K>, value: V) -> Option<V> {
         let mut iteratee = self;
         for fragment in path {
-            iteratee = iteratee.branches.entry(fragment).or_insert(Box::new(Node::new()));
+            iteratee = iteratee.branches.entry(*fragment).or_insert(Box::new(PathTree::new()));
         }
         let old_value = iteratee.leaf;
         iteratee.leaf = Some(value);
@@ -41,18 +43,26 @@ impl<K,V> Node<K,V>
 
 #[cfg(test)]
 mod tests {
-    use crate::Node;
+    use crate::PathTree;
 
     #[test]
     fn insert_get() {
-        let mut puu = Node::new();
-        let result = puu.insert(vec!["eka", "toka", "vika"], 42);
+        let mut puu = PathTree::new();
+        let result = puu.insert(&vec!["eka", "toka", "vika"], 42);
         assert_eq!(result, None);
-        let result = puu.get(vec!("eka", "toka", "vika"));
+        let result = puu.get(&vec!["eka", "toka", "vika"]);
         assert_eq!(result.unwrap(), 42);
-        let result = puu.insert(vec!["eka", "toka", "vika"], 13);
+        let result = puu.insert(&vec!["eka", "toka", "vika"], 13);
         assert_eq!(result.unwrap(), 42);
-        let result = puu.get(vec!("eka", "toka", "vika"));
+        let result = puu.get(&vec!["eka", "toka", "vika"]);
         assert_eq!(result.unwrap(), 13);
+        let result = puu.insert(&vec!["eka", "kolmas"], 12);
+        assert_eq!(result, None);
+        let result = puu.get(&vec!["eka", "kolmas"]);
+        assert_eq!(result.unwrap(), 12);
+        let result = puu.insert(&vec!["eka", "toka", "vika", "taas"], 33);
+        assert_eq!(result, None);
+        let result = puu.get(&vec!["eka", "toka", "vika", "taas"]);
+        assert_eq!(result.unwrap(), 33);
     }
 }
