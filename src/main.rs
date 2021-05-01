a701f7e38a29f7d8c4bd12db2dbd0e49b4074cdb
Otto Harju <otto.harju@gmail.com>
2021-05-01 20:57:43 +0300

    Make main async

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use decipher::get_operation;
 use decipher::ActionResult;
 use decipher::Operation::{Delete, Get, Post};
 use ephemeral::PathTree;
-use std::rc::Rc;
+use futures::executor::block_on;
+use std::{
+    rc::Rc,
+    sync::{Arc, RwLock},
+};
 
 struct Storage {
     storage: PathTree<String, bool>,
@@ -34,12 +38,21 @@ impl Storage {
     }
 }
 
+async fn do_operation(op: &str, storage: Arc<RwLock<Storage>>) {
+    (*storage.write().expect("can't get lock")).act(get_operation(op));
+}
+
 /// Demonstration of HTTP parsing and storage functions.
 /// POST method sets a true value at a path.
 /// GET method displays the value at a path.
+async fn async_operations() {
+    let storage = Arc::new(RwLock::new(Storage::new()));
+    let post = do_operation("POST /foo/bar/baz HTTP/1.1", storage.clone());
+    let get = do_operation("GET /foo/bar/baz HTTP/1.1", storage.clone());
+    let get_none = do_operation("GET /foo/bar/none HTTP/1.1", storage);
+    futures::join!(post, get, get_none);
+}
+
 fn main() {
-    let mut storage = Storage::new();
-    storage.act(get_operation("POST /foo/bar/baz HTTP/1.1"));
-    storage.act(get_operation("GET /foo/bar/baz HTTP/1.1"));
-    storage.act(get_operation("GET /foo/bar/none HTTP/1.1"));
+    block_on(async_operations());
 }
