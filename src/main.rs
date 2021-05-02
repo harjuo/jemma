d12121edf8f9b62236305286fc61bdf64b235a87
Otto Harju <otto.harju@gmail.com>
2021-05-02 18:43:21 +0300

    Parsing port number, comments added

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use futures::{
     prelude::*,
 };
 use std::{
+    env,
     io::prelude::*,
     net::{TcpListener, TcpStream},
     sync::{Arc, RwLock},
@@ -23,6 +24,9 @@ enum Error {
     Other,
 }
 
+// Stores boolean values for resources that are defined by a path
+// of strings. A GET operation to "/this/is/test" stores a true
+// value to a resource identified by ["this", "is", "test"].
 impl Storage {
     fn new() -> Storage {
         Storage {
@@ -91,8 +95,8 @@ fn handle_client(
     Ok(())
 }
 
-async fn listener(storage: Arc<RwLock<Storage>>) -> Result<(), Error> {
-    let listener = TcpListener::bind("127.0.0.1:80").map_err(Error::IoError)?;
+async fn listener(storage: Arc<RwLock<Storage>>, port: u16) -> Result<(), Error> {
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).map_err(Error::IoError)?;
     let runner = ThreadPool::new().expect("failed to create thread pool");
     for request in listener.incoming() {
         handle_client(
@@ -104,9 +108,35 @@ async fn listener(storage: Arc<RwLock<Storage>>) -> Result<(), Error> {
     Ok(())
 }
 
+// Get port to run on from the sole optional command line argument.
+// Failure to parse exits the process.
+fn get_port() -> u16 {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        match args[1].parse::<u64>() {
+            Ok(input) => {
+                if input > std::u16::MAX.into() {
+                    println!("invalid port number: {}", input);
+                    std::process::exit(1);
+                }
+                input as u16
+            }
+            Err(_) => {
+                println!("invalid port: {}", args[1]);
+                std::process::exit(1);
+            }
+        }
+    } else {
+        8080
+    }
+}
+
+// Blocks and waits for incoming connections on the port given as a command
+// line argument, or the default port. GET, DELETE and POST operations are
+// supported on resources pointed to by paths. Stored values are booleans.
 fn main() {
     block_on(
-        listener(Arc::new(RwLock::new(Storage::new()))).map(|result| {
+        listener(Arc::new(RwLock::new(Storage::new())), get_port()).map(|result| {
             if let Err(e) = result {
                 println!("There was failure: {:?}", e);
             }
